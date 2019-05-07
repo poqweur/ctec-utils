6d87ac42b70c35d9712e4cd0445713b032e4f7ef
wjy <1149447019@qq.com>
2019-05-07 12:36:54 +0800

    更新代码查看路径

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     url='https://github.com/poqweur/ctec-utils.git',
     # install_requires=[str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],  # 所需的运行环境
     install_requires=[
+        "msgpack==0.5.6",
         "pika==0.13.1",
         "requests==2.20.0",
         "DBUtils==1.3",
