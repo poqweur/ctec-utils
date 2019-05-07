ba70ae8924d77e073eafaa802c6d0fcd0282ac14
wjy <1149447019@qq.com>
2019-05-07 14:05:19 +0800

    修改引用问题

--- ctec_utils/__init__.py
+++ ctec_utils/__init__.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 # Author: Wjy
 import sqlalchemy
-import Request
-from Ampq import *
-from Database import *
-from Models import *
+from .Request import *
+from .Ampq import *
+from .Database import *
+from .Models import *
 
 __all__ = ["AsyncPublish", "Publish", "OraclePool", "RedisCluster", "MongodbCluster", "sqlalchemy", "MysqlPool",
            "Request", "JournalLog", "InternalLog", "ExternalInterfaceLoggingEvent", "OrderJournalEvent",
