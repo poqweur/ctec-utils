13681aecd620a27018ecc1643fc360b8ed23f2f6
wjy <1149447019@qq.com>
2019-05-14 14:16:15 +0800

    记录请求时差

--- ctec_utils/Request.py
+++ ctec_utils/Request.py
@@ -34,9 +34,11 @@ def get(url, params: dict = None, header: dict = None, log=None, timeout: int =5
                              "537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
     if header:
         headers.update(header)
+    # 创建外部流水日志对西昂
     journal_log = InsideOutside(transaction_id=transaction_id, dialog_type="out", address=url, http_method="GET",
                                 key_param=str(params), request_payload=str(params), request_headers=headers)
-    journal_log.request_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    # 记录请求前时间
+    journal_log.request_time = datetime.datetime.now()
     try:
         request = requests.Session()
         # http请求最多重试3次
@@ -47,21 +49,35 @@ def get(url, params: dict = None, header: dict = None, log=None, timeout: int =5
     except Exception as e:
         code, response = 1, "接口调用异常url={}, params={}, 异常信息={}".format(url, params, traceback.format_exc())
     else:
+        # 记录响应头
         journal_log.response_headers = str(response.headers)
+        # 记录响应状态
         journal_log.response_code = response.status_code
         try:
+            # 尝试转换json数据
             response = response.json()
         except ValueError:
+            # 非json数据
             response = response.content
     finally:
-        journal_log.response_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        # 记录响应时间
+        journal_log.response_time = datetime.datetime.now()
+        # 记录响应数据
         journal_log.response_payload = str(response)
-
+        # 记录请求时差
+        jet_lag = (journal_log.response_time - journal_log.request_time).total_seconds()
+        # 将请求时间对象转成字符串
+        journal_log.request_time = journal_log.request_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+        # 将响应时间对西昂转成字符串
+        journal_log.response_time = journal_log.response_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+        print("响应时间", jet_lag)
         if log:
-            log.debug("end url_params= {}, response={}".format(params, response))
             if is_external and getattr(log, "external_log"):
                 journal_log.res_content = response
-                log.external(extra=journal_log.__dict__)
+                log.external(msg="end params= {}, response={}=, 响应时间={}".format(params, response, jet_lag),
+                             extra=journal_log.__dict__)
+            else:
+                log.debug("end params= {}, response={}, 响应时间={}".format(params, response, jet_lag))
 
         return code, response
 
@@ -91,9 +107,11 @@ def post(url, data=None, params: dict = None, header: dict = None, log=None, tim
                              "537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
     if header:
         headers.update(header)
-
+    # 创建外部流水对象
     journal_log = InsideOutside(transaction_id=transaction_id, dialog_type="out", address=url, http_method="POST",
                                 key_param=str(data), request_payload=str(data), request_headers=headers)
+    # 记录请求前时间
+    journal_log.request_time = datetime.datetime.now()
     try:
         request = requests.Session()
         # http请求最多重试3次
@@ -105,18 +123,32 @@ def post(url, data=None, params: dict = None, header: dict = None, log=None, tim
         code, response = 1, "接口调用异常url={}, data={}, params={}, 异常信息={}".format(url, data,
                                                                                params, traceback.format_exc())
     else:
+        # 记录响应状态
         journal_log.response_code = response.status_code
+        # 记录响应头
         journal_log.response_headers = response.headers
         try:
+            # 尝试直接做json转换
             response = response.json()
         except ValueError:
+            # 非json响应数据给原始数据
             response = response.content
     finally:
-        journal_log.response_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        # 记录响应时间
+        journal_log.response_time = datetime.datetime.now()
+        # 记录响应数据
         journal_log.response_payload = str(response)
+        # 记录请求时差
+        jet_lag = (journal_log.response_time - journal_log.request_time).total_seconds()
+        # 将请求时间对象转成字符串
+        journal_log.request_time = journal_log.request_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+        # 将响应时间对西昂转成字符串
+        journal_log.response_time = journal_log.response_time.strftime("%Y-%m-%d %H:%M:%S.%f")
         if log:
-            log.debug("end data= {}, response={}".format(data, response))
             if is_external and getattr(log, "external_log"):
                 journal_log.res_content = response
-                log.external(extra=journal_log.__dict__)
+                log.external(msg="end data= {}, response={}, 响应时间={}".format(data, response, jet_lag),
+                             extra=journal_log.__dict__)
+            else:
+                log.debug("end data= {}, response={}, 响应时间={}".format(data, response, jet_lag))
         return code, response
